0e6b402b5e14095d212ed269e9d3d4b53652af50
Sujan Nag <nags@tcd.ie>
2018-03-10 11:06:56 +0000

    Added code to push data onto AWS DynamoDB from AWS Lambda

--- open_data/index.js
+++ open_data/index.js
@@ -1,39 +1,72 @@
 /* Collects data from open source via GET request. */
+//'use strict';
 
+var AWS = require('aws-sdk');
 const kelvinToCelsius = require('kelvin-to-celsius');
 var request = require('request');
 var fs = require('fs');
 
+const documentClient = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
+
 /* 
-Requires an APPID, received after registering. 
-Requires the name of the city.
+	Requires an APPID, received after registering. 
+	Requires the name of the city.
 */
 var file = fs.readFileSync('APPID_FILE', 'utf8');
-obj_file = JSON.parse(file);
+var obj_file = JSON.parse(file);
 
 var APPID = obj_file.APPID;
 var city = obj_file.City;
-
-url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&" + "APPID=" + APPID;
-//console.log(url);
-
-// Use request to make a GET call and collect the data
-request(
-    {
-        url : url,
-        method: "GET",
-    },
-    function (error, response, body) {
-        console.log('error:', error);
-  		console.log('statusCode:', response && response.statusCode);
-  		console.log('body:', body);
-
-  		// Parse the JSON Object
-  		var obj = JSON.parse(body);
-  		//console.log("obj:", obj);
-  		console.log("Temperature:", kelvinToCelsius(obj.main.temp));
-    }
-);
+var url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&" + "APPID=" + APPID;
+// var db_items = {
+// 	temperature: 0
+// };
+GLOBAL.temperature = 0;	
+console.log(global);
 
 
-// Push the data onto DynamoDB
+exports.handler = function(event, context, callback){
+
+
+	// Use request to make a GET call and collect the data
+	request(
+	    {
+	        url : url,
+	        method: "GET",
+	    },
+	    function (error, response, body) {
+	        console.log('error:', error);
+	  		console.log('statusCode:', response && response.statusCode);
+	  		console.log('body:', body);
+
+	  		// Parse the JSON Object
+	  		var obj = JSON.parse(body);
+	  		GLOBAL.temperature = kelvinToCelsius(obj.main.temp);
+	  		console.log("Temperature:", GLOBAL.temperature);
+    	}
+	);
+	console.log(GLOBAL.temperature);
+	// Push the data onto DynamoDB
+	var params = {
+		
+		Item: {
+			Date: Date.now(),
+			Temperature: GLOBAL.temperature
+		},
+		
+		TableName: 'Open-Data'
+	};
+	
+	documentClient.put(params, function(err, data) 
+		{
+			if(err){
+				console.log('err', err);
+				callback(err, null);
+			}
+			else{
+				callback(null, data);
+			}
+		
+		}
+	);
+};
